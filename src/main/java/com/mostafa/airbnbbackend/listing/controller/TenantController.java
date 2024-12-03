53b7aaa471fb1636f0d1225a9145e592050ddb3e
m0stafa7med <mostafadarwesh610@gmail.com>
2024-12-03 02:57:04 +0200

    Add tenant controller

---
+++ src/main/java/com/mostafa/airbnbbackend/listing/controller/TenantController.java
@@ -0,0 +1,54 @@
+package com.mostafa.airbnbbackend.listing.controller;
+
+
+import com.mostafa.airbnbbackend.listing.dto.DisplayCardListingDTO;
+import com.mostafa.airbnbbackend.listing.dto.DisplayListingDTO;
+import com.mostafa.airbnbbackend.listing.dto.SearchDTO;
+import com.mostafa.airbnbbackend.listing.enums.BookingCategory;
+import com.mostafa.airbnbbackend.listing.service.TenantService;
+import com.mostafa.airbnbbackend.shared.dto.State;
+import com.mostafa.airbnbbackend.shared.dto.StatusNotification;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/tenant-listing")
+public class TenantController {
+
+    private final TenantService tenantService;
+
+
+    public TenantController(TenantService tenantService) {
+        this.tenantService = tenantService;
+    }
+
+    @GetMapping("/get-all-by-category")
+    public ResponseEntity<Page<DisplayCardListingDTO>> findAllByBookingCategory(Pageable pageable,
+                                                                                @RequestParam BookingCategory category) {
+        return ResponseEntity.ok(tenantService.getAllByCategory(pageable, category));
+    }
+
+    @GetMapping("/get-one")
+    public ResponseEntity<DisplayListingDTO> getOne(@RequestParam UUID publicId) {
+        State<DisplayListingDTO, String> displayListingState = tenantService.getOne(publicId);
+        if (displayListingState.getStatus().equals( StatusNotification.OK)) {
+            return ResponseEntity.ok(displayListingState.getValue());
+        } else {
+            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, displayListingState.getError());
+            return ResponseEntity.of(problemDetail).build();
+        }
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<Page<DisplayCardListingDTO>> search(Pageable pageable,
+                                                              @Valid @RequestBody SearchDTO searchDTO) {
+        return ResponseEntity.ok(tenantService.search(pageable, searchDTO));
+    }
+}
